3cf69f16b097e71c4a0f3d02eaeb4f2bceb8b7b5
oktonix <xanlorimer@gmail.com>
2015-04-24 11:51:34 -0400

    Updated multiEMGServoTest
    Added fingerControl, wave, open, shut, pinch, and emgCheck methods.
    Cleaned up notes and code. Added todo list.
    
    Method list & info:
    
    setup - basic setup method
    loop - main loop
    getSerial - gets input from the serial connection
    emgCheck - checks the EMG for threshold triggering
    indexPinch - pinches using index and thumb
    openHand - opens all fingers to their "open" values
    shutHand - shuts all fingers to their "shut" values
    thumbWave - finger wave starting with the thumb
    pinkieWave - finger wave starting with the pinkie
    fingerControl - individualized finger control method
    commandHandler - handles all debug commands

--- Utilities/multiEMGServoTest/multiEMGServoTest.ino
+++ Utilities/multiEMGServoTest/multiEMGServoTest.ino
@@ -4,7 +4,7 @@
 // This code is published under the GNU GPL v2 license.
 
 ////////////////////////////////////////////////////////////////////////////////////////
-// THIS CODE ISN'T COMPLETELY TESTED AS OF 4/22/15. TESTED TO COMPILATION BUT NO MORE //
+// THIS CODE ISN'T COMPLETELY TESTED AS OF 4/24/15. TESTED TO COMPILATION BUT NO MORE //
 ////////////////////////////////////////////////////////////////////////////////////////
 
 #include <Servo.h>
@@ -14,7 +14,21 @@ int loopHaltAmount = 100000; // Number of loops before we stop the test. How man
 int numberOfLoops = 0; // Initialize the number of loops so that we can check for and use a stopping point.
 int loopDelay = 50; // Amount of time (in ms) for the main loop to wait.
 int waveDelay = 100; // Amount of time (in ms) between each finger moving in any wave.
+int fingerOpenDelay = 1000; // Amount of time (in ms) that it takes for the slowest finger to open.
+int fingerShutDelay = 1000; // Amount of time (in ms) that it takes for the slowest finger to shut.
 
+// Stuff for averaging:
+int emgAvgRead; // Just initializing the variable where the average is stored
+int thresholdA = 50; // Threshold for EMG A
+int thresholdB = 40; // Threshold for EMG B
+int thresholdC = 30; // Threshold for EMG C
+int averagerDelay = 5; // How long we wait before taking each average point
+int emgAvgReadCount = 5; // Number of points we collect to take the average
+int thresholdModifier = 10; // Modifier for preliminary threshold activation
+
+int fingerControlVal; // Initialze the angle that we'll use for individual finger controls.
+
+int emgA,emgB,emgC; // Where EMG data will be stored
 int shutAngle[] = {90,90,90,90,90}; // Angle when "shut" - From pinkie to thumb.
 int openAngle[] = {90,90,90,90,90}; // Angle when "open" - From pinkie to thumb.
 
@@ -24,7 +38,7 @@ String inputString;
 // Declare all of the servos
 Servo servo0; // Pinkie
 Servo servo1;
-Servo servo2;
+Servo servo2; // ...
 Servo servo3;
 Servo servo4; // Thumb
 
@@ -90,57 +104,158 @@ String getSerial() // Get new serial data if there is any
 void emgCheck()
 {
 // Read EMGs 1 and 2 to determine what to do next.
-  
+  emgA = analogRead(0);
+    if(emgA > (thresholdA - thresholdModifier))
+    {
+      for(int i=1; i < emgAvgReadCount; i++)
+      {
+        delay(averagerDelay);
+        emgAvgRead = ((emgAvgRead + analogRead(0)) / i);
+      }
+      if(emgAvgRead > thresholdA)
+        //DO ACTION
+        emgAvgRead = 0;
+    }
+  emgB = analogRead(1);
+    if(emgB > (thresholdB - thresholdModifier))
+    {
+      for(int i=1; i < emgAvgReadCount; i++)
+      {
+        delay(averagerDelay);
+        emgAvgRead = ((emgAvgRead + analogRead(0)) / i);
+      }
+      if(emgAvgRead > thresholdA)
+        //DO ACTION
+        emgAvgRead = 0;
+    }
+  emgC = analogRead(2); 
+    if(emgC > (thresholdC - thresholdModifier))
+    {
+      for(int i=1; i < emgAvgReadCount; i++)
+      {
+        delay(averagerDelay);
+        emgAvgRead = ((emgAvgRead + analogRead(0)) / i);
+      }
+      if(emgAvgRead > thresholdA)
+        //DO ACTION
+        emgAvgRead = 0;
+    }
 }
-// Action methods:
-// Based on value of EMG (x), do (y)
 
-// Control methods:
-// Raw servo control methods
+// Pinch method
+void indexPinch()
+{
+  openHand();
+  servo4.write(shutAngle[4]); 
+  servo3.write(shutAngle[3]);
+}
+
+// This lets us open the hand easily. Sets all fingers to their respective open angles.
+void openHand() 
+{
+  servo0.write(openAngle[0]); // Pinkie
+  servo1.write(openAngle[1]);
+  servo2.write(openAngle[2]); // ...
+  servo3.write(openAngle[3]);
+  servo4.write(openAngle[4]); // Thumb
+  delay(fingerOpenDelay); 
+}
+
+// Shuts the hand.
+void shutHand()
+{
+  servo0.write(shutAngle[0]); // Pinkie
+  servo1.write(shutAngle[1]);
+  servo2.write(shutAngle[2]); // ...
+  servo3.write(shutAngle[3]);
+  delay(100); // Might remove this later.
+  servo4.write(shutAngle[4]); // Thumb
+  delay(fingerShutDelay);
+}
 
 // Finger "wave" starting with the thumb (Servo 5)
 void thumbWave()
 {
-   servo4.write(shutAngle[4]); // Close first
-   delay(waveDelay); // Delay
-   servo3.write(shutAngle[3]); // Close next
-   servo4.write(openAngle[4]); // Open first
-   delay(waveDelay);
-   servo2.write(shutAngle[2]);
-   servo3.write(openAngle[3]);
-   delay(waveDelay);
-   servo1.write(shutAngle[1]);
-   servo2.write(openAngle[2]);
-   delay(waveDelay);
-   servo0.write(shutAngle[0]);
-   servo1.write(openAngle[1]);
-   delay(waveDelay);
-   servo0.write(openAngle[0]);   
+  openHand();
+  servo4.write(shutAngle[4]); // Close first
+  delay(waveDelay); // Delay
+  servo3.write(shutAngle[3]); // Close next
+  servo4.write(openAngle[4]); // Open first
+  delay(waveDelay);
+  servo2.write(shutAngle[2]);
+  servo3.write(openAngle[3]);
+  delay(waveDelay);
+  servo1.write(shutAngle[1]);
+  servo2.write(openAngle[2]);
+  delay(waveDelay);
+  servo0.write(shutAngle[0]);
+  servo1.write(openAngle[1]);
+  delay(waveDelay);
+  servo0.write(openAngle[0]);   
 }
 
 // Finger "wave" starting with the pinkie (Servo 0)
 void pinkieWave()
 {
-   servo0.write(shutAngle[0]); // Close first
-   delay(waveDelay); // Delay
-   servo1.write(shutAngle[1]); // Close next
-   servo0.write(openAngle[0]); // Open first
-   delay(waveDelay);
-   servo2.write(shutAngle[2]);
-   servo4.write(openAngle[1]);
-   delay(waveDelay);
-   servo3.write(shutAngle[3]);
-   servo4.write(openAngle[2]);
-   delay(waveDelay);
-   servo3.write(shutAngle[4]);
-   servo4.write(openAngle[3]);
-   delay(waveDelay);
-   servo3.write(openAngle[4]);  
+  servo0.write(shutAngle[0]); // Close first
+  delay(waveDelay); // Delay
+  servo1.write(shutAngle[1]); // Close next
+  servo0.write(openAngle[0]); // Open first
+  delay(waveDelay);
+  servo2.write(shutAngle[2]);
+  servo4.write(openAngle[1]);
+  delay(waveDelay);
+  servo3.write(shutAngle[3]);
+  servo4.write(openAngle[2]);
+  delay(waveDelay);
+  servo3.write(shutAngle[4]);
+  servo4.write(openAngle[3]);
+  delay(waveDelay);
+  servo3.write(openAngle[4]);  
 }
 
+// This method is definitely not the best. Doesn't matter too much though, since this is just for testing.
 void fingerControl()
 {
+  Serial.println("Enter the determinant servo value (0-4) and the angle to be written (0-180). Enter 9999 to exit to main loop.");
+  fingerControlVal = getSerial().toInt(); // Set value to an int
   
+  // For each thousand, a different servo is selected. We use the leading digit to determine which servo we're controlling, and the remaining three digits
+  // to determine what angle we'll write.
+  if(fingerControlVal <= 180 && fingerControlVal >= 000)  
+  {
+    fingerControlVal = fingerControlVal - 0;
+    servo0.write(fingerControlVal);
+  }
+  if(fingerControlVal <= 1180 && fingerControlVal >= 1000)  
+  {
+    fingerControlVal = fingerControlVal - 1000;
+    servo1.write(fingerControlVal);
+  }
+  if(fingerControlVal <= 2180 && fingerControlVal >= 2000) 
+  {
+    fingerControlVal = fingerControlVal - 2000;
+    servo2.write(fingerControlVal);
+  }
+  if(fingerControlVal <= 3180 && fingerControlVal >= 3000)  
+  {
+    fingerControlVal = fingerControlVal - 3000;
+    servo3.write(fingerControlVal);
+  }
+  if(fingerControlVal <= 4180 && fingerControlVal >= 4000)  
+  {
+    fingerControlVal = fingerControlVal - 4000;
+    servo4.write(fingerControlVal);
+  }
+  if(fingerControlVal == 9999)
+  {
+    loop();
+  } 
+  else
+  {
+    Serial.println("Invalid selection.\n\n\n");
+    fingerControl();
+  }
 }
 
 // Command Handler
@@ -182,33 +297,48 @@ void commandHandler(String command)
     {
       fingerControl();
     }
+    if(inputString.equalsIgnoreCase("PNCH")) 
+    {
+      indexPinch();
+    }
+    if(inputString.equalsIgnoreCase("HELP")) 
+    {
+      Serial.println("Available: OPEN, SHUT, RSTL, TWAV, PWAV, FING, HELP, PNCH, HELP.");
+      delay(3000);
+    }
     else 
     {
-      Serial.println("Unknown Command. Available: DONE, OPEN, SHUT, RSTL, TWAV, PWAV, FING"); //Handle unknown commands
+      Serial.println("Unknown Command. Type 'HELP' for more info."); //Handle unknown commands
     }
   inputString = ""; // Then we clear the string
 }
 
 
-
 /*
 Notes:
 
-In the electronics portion of this, perhaps we should have a transistor allowing power to pass to the servos. 
-If something bad happens, the servos can all go limp as the transistor is turned off.
-We could implement this by adding an emergency stop button.
+TODO: Add a kill switch or button.
+TODO: Power servos, arduino, and MSK separately. Two 5V lines (a steady one for the Arduino and one for the servos), and a stead 9V for the MSK.
+TODO: Test the indexPinch method - need to know if the fingers close at the same time or if they have some delay.
+TODO: Log serial data to a file through processing. 
+TODO: Add processing comms code (A(n), B(n), C(n))
+TODO: Re-evaluate the necessity of having loop(); in certain places.
+TODO: Implement averaging of EMG data
+TODO: Basic testing of all methods.
+TODO: Create a list of the methods contained herein.
 
-But yeah, the servos should have a separate power supply.
+Method list:
 
-At this point, why not just design a simple power circuit? We'll need a high-availability 5V rail for the Arduino, as well as a rail for the servos.
-A third, stable 9v rail will be needed for the EMGs. Ask Larry about this.
+setup - basic setup method
+loop - main loop
+getSerial - gets input from the serial connection
+emgCheck - checks the EMG for threshold triggering
+indexPinch - pinches using index and thumb
+openHand - opens all fingers to their "open" values
+shutHand - shuts all fingers to their "shut" values
+thumbWave - finger wave starting with the thumb
+pinkieWave - finger wave starting with the pinkie
+fingerControl - individualized finger control method
+commandHandler - handles all debug commands
 
-Oh, we also want to log all of this information to a file, probably. Maybe we should take a video as well, to associate activations with movements.
-
-What should our actions be? I guess hand open, hand close. Maybe something to control groups of fingers? pinch?
-Some inputtable states like "Wave" would be cool. Stuff to idle with. *WAVE IMPLEMENTED
-
-Major TODO:
-  Make SerialEvent return a String so that we can have more versatility when calling it. As it is now, this is really inefficient :/ -- DONE
-  
 */ 
